94fb6a7dede7a057db4f65d91c889e5fa45e967a
deekshitha <atellydeekshitha6@gmail.com>
2025-11-05 13:13:53 +0530

    Added removeduplicates.py in Arrays folder

--- Arrays/frequencyofelement.py
+++ Arrays/frequencyofelement.py
@@ -1,8 +1,16 @@
+
 arr = [1,2,3,1,2,34,4,5,2]
-freq = {}
+new_array = []
 for i in range(len(arr)):
-    if arr[i] in freq:
-        freq[arr[i]]+=1
-    else :
-        freq[arr[i]]= 1
-print(freq)
+    if arr[i] not in new_array:
+        new_array.append(arr[i])
+print(new_array)
+
+nums = [1,1,2,2,3,3]
+left = 0
+while left < len(nums)-1:
+    if nums[left] == nums[left+1]:
+        nums.pop(left+1)
+    else:
+        left+=1
+print(nums)
